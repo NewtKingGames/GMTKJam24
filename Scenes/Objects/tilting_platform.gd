92efa3d593f6f20cad3f83ebbd84408430e165b3
NewtKingGames <newtkinggames@gmail.com>
2024-08-17 19:01:40 -0500

    Tweaks to tilting platform

--- Scenes/Objects/tilting_platform.gd
+++ Scenes/Objects/tilting_platform.gd
@@ -27,11 +27,11 @@ func _process(delta):
 # 1 spins clockwise -1 spins counter clockwise
 func rotate_platform(direction: int, delta: float):
 	if direction > 0:
-		set_rotation_degrees(lerpf(rotation_degrees, max_rotation_right, 0.5 * delta))	
+		set_rotation_degrees(lerpf(rotation_degrees, max_rotation_right, 1 * delta))	
 	elif direction < 0:
-		set_rotation_degrees(lerpf(rotation_degrees, max_rotation_left, 0.5 * delta))
+		set_rotation_degrees(lerpf(rotation_degrees, max_rotation_left, 1 * delta))
 	else:
-		set_rotation_degrees(lerpf(rotation_degrees, 0, 0.4 * delta))
+		set_rotation_degrees(lerpf(rotation_degrees, 0, 0.6 * delta))
 
 
 func _on_right_area_weight_changed(weight: float):
